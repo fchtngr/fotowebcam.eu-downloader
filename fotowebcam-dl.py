46d92cbcce7431fe4f3430b6c9572d6c7a16770b
Thomas Feichtinger <thomas.feichtinger@catalysts.cc>
2017-02-12 13:51:43 +0100

    refactor: more functions, less globals :star2:

--- fotowebcam-dl.py
+++ fotowebcam-dl.py
@@ -7,88 +7,109 @@ import os
 import argparse
 import sys
 
-###
-### Downloads images from a fotowebcam.eu webcam. 
-###
-
+IMAGE_URL_PATTERN = "http://www.foto-webcam.eu/webcam/%s/"
+BESTOF_URL_PATTERN = "http://www.foto-webcam.eu/webcam/include/list.php?img=&wc=%s&bestof=1"
+CACHE_PATH_PATTERN = ".%s_cache.json"
 RESOLUTION = "_hu"
 EXTENSION = ".jpg"
+PATH = os.getcwd()
 
-parser = argparse.ArgumentParser(description="Downloads bestof images from a fotowebcam.eu webcam")
-parser.add_argument('webcam', help='the fotowebcam name')
-parser.add_argument('--path', help='where to save the images, default is cwdir')
+def print_result(nr_bestofimages, nr_downloaded, nr_blacklisted, nr_existing, added_to_blacklist):
+    print ""
+    print "-" * 75
+    print "# of best-of images:\t%d" % nr_bestofimages
+    print "-" * 75
+    print "Downloaded: \t\t%d" % nr_downloaded
+    print "Ignored: \t\t%d (blacklist: %d, existing: %d)" % (nr_blacklisted + nr_existing, nr_blacklisted, nr_existing)
+    if len(added_to_blacklist) > 0:
+        print "Added to blacklist: \t%d" % len(added_to_blacklist)
+        print "\t%s" % str(added_to_blacklist) 
+    print "-" * 75
 
-if len(sys.argv)==1:
-    parser.print_help()
-    sys.exit(1)
+def parse_args():
+    parser = argparse.ArgumentParser(description="Downloads bestof images from a fotowebcam.eu webcam")
+    parser.add_argument('webcam', help='the fotowebcam name')
+    parser.add_argument('--path', help='where to save the images, default is cwdir')
 
-args = parser.parse_args()
+    if len(sys.argv)==1:
+        parser.print_help()
+        sys.exit(1)
 
-URL = ("http://www.foto-webcam.eu/webcam/%s/" % args.webcam)
+    args = parser.parse_args()
 
-targetpath = os.dirname(args.path) if args.path else os.getcwd()
-blacklistpath = os.path.join(targetpath, "blacklist")
-fileslistpath = os.path.join(targetpath, "fileslist")
+    if (args.path):
+        PATH = os.dirname(args.path)
 
-imagelist = requests.get("http://www.foto-webcam.eu/webcam/include/list.php?img=&wc=%s&bestof=1" % args.webcam)
-images = json.loads(imagelist.text)
+    return args
 
-bestoflist = images['bestof']
+def write_cache(name, cache):
+    cache_file = os.path.join(PATH, (CACHE_PATH_PATTERN % name))
+    with open(cache_file, 'w') as f:
+        f.write(json.dumps(cache))
 
-with open(blacklistpath, 'a+') as f:
-	blacklist = f.read().splitlines()
+def read_cache(name):
+    cache_file = os.path.join(PATH, (CACHE_PATH_PATTERN % name))
+    with open(cache_file, 'r') as f:
+        cache = json.loads(f.read())        
+    return cache
 
-with open(fileslistpath, 'a+') as f:
-	addToBlackList = f.read().splitlines()
+def load_imagelist(name):
+    imagelist = requests.get(BESTOF_URL_PATTERN % name)
+    images = json.loads(imagelist.text)
+    return images['bestof']
 
-for f in os.listdir(targetpath):
-	if f in addToBlackList:
-		addToBlackList.remove(f)
+def update_blacklist(cache):
+    blacklist = cache["blacklist"]
+    add_to_blacklist = list(cache["files"])
+    for f in os.listdir(PATH):
+        if f in add_to_blacklist:
+            add_to_blacklist.remove(f)
 
-blacklist = blacklist + addToBlackList
+    cache["blacklist"].append(add_to_blacklist)
+    return add_to_blacklist
+
+def download(name, bestoflist, cache):
+    nr_downloaded = nr_blacklisted = nr_existing = 0
+    downloadlist = []
+    for i in bestoflist:
+        imagename = i + RESOLUTION + EXTENSION
+        imageurl = (IMAGE_URL_PATTERN % name) + imagename
+
+        filename = imagename.replace("/", "-")
+        filepath = os.path.join(PATH, filename)
+        
+        print "checking %s:" % imagename
+        
+        if os.path.isfile(filepath):
+            print "\talready exists!"
+            downloadlist.append(filename)
+            nr_existing += 1
+        elif filename in cache["blacklist"]:
+            print "\tblacklisted!"
+            nr_blacklisted += 1
+        else:
+            print "\tdownloading..."
+            urllib.urlretrieve(imageurl, filepath)
+            downloadlist.append(filename)
+            nr_downloaded += 1
+            print "\tdone"
     
-downloaded = 0
-blacklisted = 0
-existing = 0
+    cache["files"] = downloadlist
     
-downloadlist = []
-for i in bestoflist:
-    imagename = i + RESOLUTION + EXTENSION
+    return (nr_downloaded, nr_blacklisted, nr_existing)
 
-    imageurl = URL + imagename
-    filename = imagename.replace("/", "-")
-    filepath = os.path.join(targetpath, filename)
+def main():
+    args = parse_args()
+
+    bestoflist = load_imagelist(args.webcam)
+
+    cache = read_cache(args.webcam)
+    added_to_blacklist = update_blacklist(cache)
+
+    (nr_downloaded, nr_blacklisted, nr_existing) = download(args.webcam, bestoflist, cache)
     
-    print "checking %s:" % imagename
-    #print imageurl
-    
-    if os.path.isfile(filepath):
-        print "\talready exists!"
-        downloadlist.append(filename)
-        existing = existing + 1
-    elif filename in blacklist:
-        print "\tblacklisted!"
-        blacklisted = blacklisted + 1
-    else:
-        print "\tdownloading..."
-        urllib.urlretrieve(imageurl, filepath)
-        downloadlist.append(filename)
-        downloaded = downloaded + 1
-        print "\tdone"
+    write_cache(args.webcam, cache)
+    print_result(len(bestoflist), nr_downloaded, nr_blacklisted, nr_existing, added_to_blacklist)
 
-with open(os.path.join(targetpath, 'fileslist'), 'w') as f:
-	f.write("\n".join(downloadlist))
-
-with open(blacklistpath, 'w') as f:
-	f.write("\n".join(blacklist))
-
-print ""
-print "-" * 50
-print "BestOf imgs available:\t%d" % len(bestoflist)
-print "-" * 50
-print "Downloaded: \t\t%d" % downloaded
-print "Ignored: \t\t%d (blacklist: %d, existing: %d)" % (blacklisted + existing, blacklisted, existing)
-if len(addToBlackList) > 0:
-	print "Added to blacklist: \t%d" % len(addToBlackList)
-	print "\t%s" % str(addToBlackList) 
-print "-" * 50
+if __name__ == "__main__":
+    main()
