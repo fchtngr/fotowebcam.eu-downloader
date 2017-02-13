bb9fb6e24d0059d2f3c89f4121a43151e7b276b8
Thomas Feichtinger <thomas.feichtinger@catalysts.cc>
2017-02-13 21:41:01 +0100

    rename to fotowebcamdl.py and fix some linter warnings

--- fotowebcamdl.py
+++ fotowebcamdl.py
@@ -1,11 +1,12 @@
-from lxml import html
-from lxml import etree
+"""
+A script for downloading best-of images from www.fotowebcam.eu
+"""
+import argparse
 import json
-import requests
 import urllib
 import os
-import argparse
 import sys
+import requests
 
 IMAGE_URL_PATTERN = "http://www.foto-webcam.eu/webcam/%s/"
 BESTOF_URL_PATTERN = "http://www.foto-webcam.eu/webcam/include/list.php?img=&wc=%s&bestof=1"
@@ -15,23 +16,28 @@ EXTENSION = ".jpg"
 PATH = os.getcwd()
 
 def print_result(nr_bestofimages, nr_downloaded, nr_blacklisted, nr_existing, added_to_blacklist):
+    """
+    Print the result.
+    """
     print ""
     print "-" * 75
     print "# of best-of images:\t%d" % nr_bestofimages
     print "-" * 75
     print "Downloaded: \t\t%d" % nr_downloaded
-    print "Ignored: \t\t%d (blacklist: %d, existing: %d)" % (nr_blacklisted + nr_existing, nr_blacklisted, nr_existing)
+    print "Ignored: \t\t%d (blacklist: %d, existing: %d)" % (nr_blacklisted + nr_existing,
+                                                             nr_blacklisted, nr_existing)
     if len(added_to_blacklist) > 0:
         print "Added to blacklist: \t%d" % len(added_to_blacklist)
-        print "\t%s" % str(added_to_blacklist) 
+        print "\t%s" % str(added_to_blacklist)
     print "-" * 75
 
 def parse_args():
-    parser = argparse.ArgumentParser(description="Downloads bestof images from a fotowebcam.eu webcam")
+    parser = argparse.ArgumentParser(
+        description="Downloads bestof images from a fotowebcam.eu webcam")
     parser.add_argument('webcam', help='the fotowebcam name')
     parser.add_argument('--path', help='where to save the images, default is cwdir')
 
-    if len(sys.argv)==1:
+    if len(sys.argv) == 1:
         parser.print_help()
         sys.exit(1)
 
@@ -44,13 +50,13 @@ def parse_args():
 
 def write_cache(name, cache):
     cache_file = os.path.join(PATH, (CACHE_PATH_PATTERN % name))
-    with open(cache_file, 'w') as f:
-        f.write(json.dumps(cache))
+    with open(cache_file, 'w') as cache_file:
+        cache_file.write(json.dumps(cache))
 
 def read_cache(name):
     cache_file = os.path.join(PATH, (CACHE_PATH_PATTERN % name))
-    with open(cache_file, 'r') as f:
-        cache = json.loads(f.read())        
+    with open(cache_file, 'r') as cache_file:
+        cache = json.loads(cache_file.read())
     return cache
 
 def load_imagelist(name):
@@ -59,11 +65,10 @@ def load_imagelist(name):
     return images['bestof']
 
 def update_blacklist(cache):
-    blacklist = cache["blacklist"]
     add_to_blacklist = list(cache["files"])
-    for f in os.listdir(PATH):
-        if f in add_to_blacklist:
-            add_to_blacklist.remove(f)
+    for file_name in os.listdir(PATH):
+        if file_name in add_to_blacklist:
+            add_to_blacklist.remove(file_name)
 
     cache["blacklist"].append(add_to_blacklist)
     return add_to_blacklist
@@ -77,9 +82,9 @@ def download(name, bestoflist, cache):
 
         filename = imagename.replace("/", "-")
         filepath = os.path.join(PATH, filename)
-        
+
         print "checking %s:" % imagename
-        
+
         if os.path.isfile(filepath):
             print "\talready exists!"
             downloadlist.append(filename)
@@ -93,9 +98,9 @@ def download(name, bestoflist, cache):
             downloadlist.append(filename)
             nr_downloaded += 1
             print "\tdone"
-    
+
     cache["files"] = downloadlist
-    
+
     return (nr_downloaded, nr_blacklisted, nr_existing)
 
 def main():
@@ -107,9 +112,10 @@ def main():
     added_to_blacklist = update_blacklist(cache)
 
     (nr_downloaded, nr_blacklisted, nr_existing) = download(args.webcam, bestoflist, cache)
-    
+
     write_cache(args.webcam, cache)
     print_result(len(bestoflist), nr_downloaded, nr_blacklisted, nr_existing, added_to_blacklist)
 
 if __name__ == "__main__":
     main()
+    
